86023f6624eb656d57bf5a97d2796b354de4deb2
cathyspirit <cathyspirit@gmail.com>
2022-01-03 19:41:07 +0530

    B added code

--- src/practice/ForGit.java
+++ src/practice/ForGit.java
@@ -2,9 +2,14 @@ package practice;
 
 public class ForGit {
 	
+	private void m2() {
+		System.out.println("Method 2 - by B");
+
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Git");
+		//System.out.println("Git");
 		int i = 0;
 		System.out.println(i);
 	}
